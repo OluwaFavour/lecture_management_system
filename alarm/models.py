0be66b6936bb7b848d74080af85fbfcd6ffcde64
Favour Olusayo <fsticks8187@gmail.com>
2024-08-21 21:25:13 +0100

    chore: Remove commented out code in alarm/models.py

--- alarm/models.py
+++ alarm/models.py
@@ -2,17 +2,17 @@ from django.contrib.auth import get_user_model
 from django.db import models
 
 # Create your models here.
-User = get_user_model()
+# User = get_user_model()
 
 
-class Alarm(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="alarms")
-    title = models.CharField(max_length=255)
-    description = models.TextField()
-    timestamp = models.DateTimeField(auto_now_add=True)
-    is_active = models.BooleanField(default=True)
-    is_seen = models.BooleanField(default=False)
+# class Alarm(models.Model):
+#     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="alarms")
+#     title = models.CharField(max_length=255)
+#     description = models.TextField()
+#     timestamp = models.DateTimeField(auto_now_add=True)
+#     is_active = models.BooleanField(default=True)
+#     is_seen = models.BooleanField(default=False)
 
-    def mark_as_seen(self):
-        self.is_seen = True
-        self.save(update_fields=["is_seen"])
+#     def mark_as_seen(self):
+#         self.is_seen = True
+#         self.save(update_fields=["is_seen"])
